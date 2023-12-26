45cc1e6c21b33de804e8aa9aeb3ec561f67898e6
Areacas <zaxarrovdima@ya.ru>
2023-12-26 14:26:28 +0300

    Создание директорий

--- src/main.py
+++ src/main.py
@@ -1,11 +1,18 @@
 import asyncio
 import logging
 from bot import start_bot
+import os
 
 logging.basicConfig(filename='info.log',
                     filemode='w',
                     level=logging.INFO,
                     format='%(name)s - %(levelname)s - %(message)s')
+
+
+directories = ["reports", "ready_report"]
+for directory in directories:
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
 
 async def main():
